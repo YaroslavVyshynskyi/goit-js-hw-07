2afbc83138f5963ab0abb5fcdc505504983d6263
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-05-01 23:09:17 +0300

    add gallery Markup/basiclightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,39 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// const gallery = document.querySelector(".gallery");
+// const galleryList = document.createElement("ul");
+// galleryList.classList.add("gallery__list");
+// gallery.append(galleryList);
+// console.log(gallery);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+console.log(createGalleryItemMarkup(galleryItems));
+
+function createGalleryItemMarkup(elements) { 
+    return elements.map(({preview, description, original}) => { 
+    return `
+        <div
+        class="gallery__item">
+        <a
+        class="gallery__link">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        data-src="${original}"
+        ></img></a></div>
+        `;
+    })
+    .join("");
+};
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+function onGalleryContainerClick(evt) { 
+    console.log(evt.target.dataset.src);
+};
+
