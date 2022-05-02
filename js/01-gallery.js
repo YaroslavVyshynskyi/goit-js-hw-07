ee50c99c1b0d6566be7b46832b70438dd682d0ae
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-05-02 23:37:18 +0300

    add  basicLightbox and close modal Esc btn

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,40 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
-// const gallery = document.querySelector(".gallery");
-// const galleryList = document.createElement("ul");
-// galleryList.classList.add("gallery__list");
-// gallery.append(galleryList);
-// console.log(gallery);
-
 const galleryContainer = document.querySelector(".gallery");
 const galleryMarkup = createGalleryItemMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
-
-console.log(createGalleryItemMarkup(galleryItems));
+galleryContainer.addEventListener("click", onGalleryContainerClick);
 
 function createGalleryItemMarkup(elements) { 
     return elements.map(({preview, description, original}) => { 
-    return `
-        <div
-        class="gallery__item">
-        <a
-        class="gallery__link">
-        <img
-        class="gallery__image"
-        src="${preview}"
-        alt="${description}"
-        data-src="${original}"
-        ></img></a></div>
-        `;
-    })
+        return `
+            <div
+            class="gallery__item">
+            <a
+            class="gallery__link">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            data-src="${original}"
+            ></img></a></div>
+            `;
+        })
     .join("");
 };
 
-galleryContainer.addEventListener("click", onGalleryContainerClick);
 function onGalleryContainerClick(evt) { 
-    console.log(evt.target.dataset.src);
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) { 
+        return
+    }
+    const originalImgSrc = evt.target.dataset.src;
+    
+    const instance = basicLightbox.create(`<img src=${originalImgSrc}>`, {
+        closable: true,
+        onShow: (instance) => { window.addEventListener('keydown', onModalPressEsc) },
+        onClose: (instance) => { window.removeEventListener('keydown', onModalPressEsc) },
+    });
+    
+    instance.show();
+
+    function onModalPressEsc(event) { 
+        if (event.key === "Escape") { 
+            instance.close();
+        }
+    }
 };
 
